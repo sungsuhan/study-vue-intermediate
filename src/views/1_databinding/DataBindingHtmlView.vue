4405d949504f424dc12bec6cb3f9c9d0e002960a
sungsuhan <swhanssu@gmail.com>
2022-08-24 22:48:26 +0900

    캡틴 판교

--- src/views/1_databinding/DataBindingHtmlView.vue
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -13,10 +13,6 @@ export default {
       htmlString: '<p style="color:red;">빨간색 문자</p>'
     }
   },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
   methods: {}
 }
 </script>
