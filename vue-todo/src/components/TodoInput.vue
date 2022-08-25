81b6a2bd1993c3be7eeb1e9fe65e29a4fd0367bd
sungsuhan <swhanssu@gmail.com>
2022-08-25 23:46:34 +0900

    input, list

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
- <div>
-   <input type="text" v-model="newTodoItem">
-   <button v-on:click="addTodo">add</button>
+ <div class="inputBox shadow">
+   <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+   <span class="addContainer" v-on:click="addTodo">
+     <i class="fa-solid fa-plus"></i>
+   </span>
  </div>
 </template>
 
@@ -14,13 +16,43 @@ export default {
   },
   methods: {
     addTodo: function () {
-      console.log(this.newTodoItem);
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.newTodoItem = '';
+      if (this.newTodoItem !== '') {
+        var obj = {completed: false, item: this.newTodoItem}
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
+    },
+    clearInput: function () {
+      this.newTodoItem = "";
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: black;
+  vertical-align: middle;
+}
+
 </style>
