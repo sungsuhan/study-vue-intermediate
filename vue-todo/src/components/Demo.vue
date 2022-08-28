db8e759d27517366cf6b28843a5b19072a8a6251
sungsuhan <swhanssu@gmail.com>
2022-08-28 17:18:35 +0900

    helper

---
+++ vue-todo/src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p> 100
+    <p>{{ doublePrice }}</p> 200
+    <p>{{ triplePrice }}</p> 300
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice'])
+  }
+}
+</script>
+
+<style>
+</style>
